28b7411adef661a33787b115059ee02b4ee4484b
Ingrid da Silva Ferreira da Rosa <136195798+ingridsfrosa@users.noreply.github.com>
2023-09-04 19:46:08 -0300

    Add files via upload

---
+++ pesquisa-satisfacao/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const classificacoes = document.querySelectorAll(".classificação");
+    const caixaClassificacao = document.querySelector(".caixa_classificação");
+    const sendBtn = document.querySelector("#send");
+    const caixa = document.querySelector("#caixa");
+    let classificacaoSelecionada = "Satisfeito";
+  
+    function handleClassificacaoClick(e) {
+      const classificacaoClicada = e.target.closest(".classificação");
+      if (!classificacaoClicada) return;
+  
+      removeActive();
+      classificacaoClicada.classList.add("active");
+      classificacaoSelecionada = classificacaoClicada.querySelector("p").textContent;
+    }
+  
+    caixaClassificacao.addEventListener("click", handleClassificacaoClick);
+  
+    sendBtn.addEventListener("click", function () {
+      caixa.innerHTML = `
+          <i class="fas fa-heart"></i>
+          <h1>Obrigada!</h1>
+          <br>
+          <strong>Feedback: ${classificacaoSelecionada}</strong>
+          <p>Agradecemos sua participação!</p>
+      `;
+    });
+  
+    function removeActive() {
+      classificacoes.forEach((classificacao) => {
+        classificacao.classList.remove("active");
+      });
+    }
+  });
+  
+  
